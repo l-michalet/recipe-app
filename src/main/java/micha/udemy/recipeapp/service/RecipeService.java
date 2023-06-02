d508fcec95bd08ab5dec70f5e7b9f90fcf5ec716
Lucas Michalet <lucas.michalet@symphony.com>
2023-06-02 22:49:01 +0200

    Get recipes

---
+++ src/main/java/micha/udemy/recipeapp/service/RecipeService.java
@@ -0,0 +1,10 @@
+package micha.udemy.recipeapp.service;
+import micha.udemy.recipeapp.model.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+
+}
