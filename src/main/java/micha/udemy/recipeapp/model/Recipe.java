43c9782a8d979953e566ac6b264cc0ccf4c34adf
Lucas Michalet <lucas.michalet@symphony.com>
2023-06-03 17:38:34 +0200

    Little refactoring

--- src/main/java/micha/udemy/recipeapp/model/Recipe.java
+++ src/main/java/micha/udemy/recipeapp/model/Recipe.java
@@ -49,6 +49,12 @@ public class Recipe {
     @JoinTable(name= "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
     public Long getId() {
         return id;
     }
@@ -135,6 +141,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Difficulty getDifficulty() {
