755d0f86132d71f28b0be7ed412a87b92c5f9b6b
Lucas Michalet <lucas.michalet@symphony.com>
2023-06-04 16:35:21 +0200

    Test with ArgumentCaptor

--- src/main/java/micha/udemy/recipeapp/model/Recipe.java
+++ src/main/java/micha/udemy/recipeapp/model/Recipe.java
@@ -1,6 +1,9 @@
 package micha.udemy.recipeapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -19,6 +22,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Recipe {
 
--- src/test/java/micha/udemy/recipeapp/controller/IndexControllerTest.java
+++ src/test/java/micha/udemy/recipeapp/controller/IndexControllerTest.java
@@ -1,18 +1,22 @@
 package micha.udemy.recipeapp.controller;
 
+import micha.udemy.recipeapp.model.Recipe;
 import micha.udemy.recipeapp.service.RecipeService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
 
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class IndexControllerTest {
@@ -28,10 +32,20 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+        when(recipeService.getRecipes()).thenReturn(Set.of(
+            Recipe.builder().id(1L).build(),
+            Recipe.builder().id(2L).build()
+        ));
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         String viewName = indexController.getIndexPage(model);
 
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
