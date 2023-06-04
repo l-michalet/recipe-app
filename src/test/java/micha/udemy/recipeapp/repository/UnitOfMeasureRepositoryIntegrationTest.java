8810f8ce21756d7276b4eff827dc335df0accb65
Lucas Michalet <lucas.michalet@symphony.com>
2023-06-04 16:50:10 +0200

    Integration test with @DataJpaTest

---
+++ src/test/java/micha/udemy/recipeapp/repository/UnitOfMeasureRepositoryIntegrationTest.java
@@ -0,0 +1,29 @@
+package micha.udemy.recipeapp.repository;
+
+import micha.udemy.recipeapp.model.UnitOfMeasure;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@DataJpaTest
+class UnitOfMeasureRepositoryIntegrationTest {
+
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    // The context is loaded and the uoms are built and saved from the data.sql file
+
+    @Test
+    public void findByDescription_teaspoon() throws Exception {
+        UnitOfMeasure uom = unitOfMeasureRepository.findByDescription("Teaspoon").orElseThrow();
+        assertEquals("Teaspoon", uom.getDescription());
+    }
+
+    @Test
+    public void findByDescription_cup() throws Exception {
+        UnitOfMeasure uom = unitOfMeasureRepository.findByDescription("Cup").orElseThrow();
+        assertEquals("Cup", uom.getDescription());
+    }
+}
