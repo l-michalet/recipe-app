83e968ecbe93a58e4463d2cc985a918d03a15d1d
Lucas Michalet <lucas.michalet@symphony.com>
2023-06-10 00:12:52 +0200

    IT test : RecipeService

---
+++ src/test/java/micha/udemy/recipeapp/service/RecipeServiceIT.java
@@ -0,0 +1,50 @@
+package micha.udemy.recipeapp.service;
+
+import micha.udemy.recipeapp.command.RecipeCommand;
+import micha.udemy.recipeapp.converter.RecipeCommandToRecipe;
+import micha.udemy.recipeapp.converter.RecipeToRecipeCommand;
+import micha.udemy.recipeapp.model.Recipe;
+import micha.udemy.recipeapp.repository.RecipeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class RecipeServiceIT {
+
+    public static final String NEW_DESCRIPTION = "New Description";
+
+    @Autowired
+    RecipeService recipeService;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @Autowired
+    RecipeCommandToRecipe recipeCommandToRecipe;
+
+    @Autowired
+    RecipeToRecipeCommand recipeToRecipeCommand;
+
+    @Transactional
+    @Test
+    public void testSaveOfDescription() throws Exception {
+        //given
+        Iterable<Recipe> recipes = recipeRepository.findAll();
+        Recipe testRecipe = recipes.iterator().next();
+        RecipeCommand testRecipeCommand = recipeToRecipeCommand.convert(testRecipe);
+
+        //when
+        testRecipeCommand.setDescription(NEW_DESCRIPTION);
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(testRecipeCommand);
+
+        //then
+        assertEquals(NEW_DESCRIPTION, savedRecipeCommand.getDescription());
+        assertEquals(testRecipe.getId(), savedRecipeCommand.getId());
+        assertEquals(testRecipe.getCategories().size(), savedRecipeCommand.getCategories().size());
+        assertEquals(testRecipe.getIngredients().size(), savedRecipeCommand.getIngredients().size());
+    }
+}
