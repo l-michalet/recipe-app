38a00d2759f57eb8ad70085ff84d23b18f261797
Lucas Michalet <lucas.michalet@symphony.com>
2023-06-09 22:20:05 +0200

    TDD: Write CategoryToCategoryCommand test

--- src/test/java/converters/CategoryToCategoryCommandTest.java
+++ src/test/java/converters/CategoryToCategoryCommandTest.java
@@ -1,4 +1,46 @@
 package converters;
 
+import micha.udemy.recipeapp.command.CategoryCommand;
+import micha.udemy.recipeapp.converter.CategoryToCategoryCommand;
+import micha.udemy.recipeapp.model.Category;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class CategoryToCategoryCommandTest {
+
+    public static final Long ID_VALUE = 1L;
+    public static final String DESCRIPTION = "description";
+
+    @InjectMocks
+    CategoryToCategoryCommand converter;
+
+    @Test
+    public void testNullObject() {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() {
+        assertNotNull(converter.convert(new Category()));
+    }
+
+    @Test
+    public void convert() {
+        //given
+        Category category = new Category();
+        category.setId(ID_VALUE);
+        category.setDescription(DESCRIPTION);
+
+        //when
+        CategoryCommand categoryCommand = converter.convert(category);
+
+        //then
+        assertEquals(ID_VALUE, categoryCommand.getId());
+        assertEquals(DESCRIPTION, categoryCommand.getDescription());
+    }
+
 }
