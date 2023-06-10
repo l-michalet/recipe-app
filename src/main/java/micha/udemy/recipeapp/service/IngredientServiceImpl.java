fc4c324522652188ef886ffc9d907e4983cfbda4
Lucas Michalet <lucas.michalet@symphony.com>
2023-06-10 12:08:41 +0200

    Update Ingredient

--- src/main/java/micha/udemy/recipeapp/service/IngredientServiceImpl.java
+++ src/main/java/micha/udemy/recipeapp/service/IngredientServiceImpl.java
@@ -2,10 +2,15 @@ package micha.udemy.recipeapp.service;
 
 import lombok.extern.slf4j.Slf4j;
 import micha.udemy.recipeapp.command.IngredientCommand;
+import micha.udemy.recipeapp.converter.IngredientCommandToIngredient;
 import micha.udemy.recipeapp.converter.IngredientToIngredientCommand;
+import micha.udemy.recipeapp.model.Ingredient;
 import micha.udemy.recipeapp.model.Recipe;
+import micha.udemy.recipeapp.model.UnitOfMeasure;
 import micha.udemy.recipeapp.repository.RecipeRepository;
+import micha.udemy.recipeapp.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,22 +19,56 @@ import java.util.Optional;
 public class IngredientServiceImpl implements IngredientService {
 
     private final IngredientToIngredientCommand ingredientToIngredientCommand;
+    private final IngredientCommandToIngredient ingredientCommandToIngredient;
     private final RecipeRepository recipeRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, IngredientCommandToIngredient ingredientCommandToIngredient, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
         this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+        this.ingredientCommandToIngredient = ingredientCommandToIngredient;
         this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
     }
 
     @Override
     public IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId) {
         Recipe recipe = recipeRepository.findById(recipeId)
-            .orElseThrow(() -> new RuntimeException("Recipe not found"));
+                .orElseThrow(() -> new RuntimeException("Recipe not found"));
 
         return recipe.getIngredients().stream()
-            .filter(ingredient -> ingredient.getId().equals(ingredientId))
-            .map(ingredientToIngredientCommand::convert)
-            .findFirst()
-            .orElseThrow(() -> new RuntimeException("Ingredient not found " + ingredientId));
+                .filter(ingredient -> ingredient.getId().equals(ingredientId))
+                .map(ingredientToIngredientCommand::convert)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Ingredient not found " + ingredientId));
+    }
+
+    @Override
+    @Transactional
+    public IngredientCommand saveIngredientCommand(IngredientCommand command) {
+        Recipe recipe = recipeRepository.findById(command.getRecipeId())
+           .orElseThrow(() -> new RuntimeException("Recipe not found for id: " + command.getRecipeId()));
+
+        Optional<Ingredient> ingredientOptional = recipe
+            .getIngredients()
+            .stream()
+            .filter(ingredient -> ingredient.getId().equals(command.getId()))
+            .findFirst();
+        if (ingredientOptional.isPresent()) {
+            Ingredient ingredientFound = ingredientOptional.get();
+            ingredientFound.setDescription(command.getDescription());
+            ingredientFound.setAmount(command.getAmount());
+            UnitOfMeasure uom = unitOfMeasureRepository.findById(command.getUom().getId())
+               .orElseThrow(() -> new RuntimeException("UOM NOT FOUND"));
+            ingredientFound.setUom(uom);
+        } else {
+            recipe.addIngredient(ingredientCommandToIngredient.convert(command));
+        }
+
+        Recipe savedRecipe = recipeRepository.save(recipe);
+
+        return ingredientToIngredientCommand.convert(savedRecipe.getIngredients().stream()
+                .filter(recipeIngredients -> recipeIngredients.getId().equals(command.getId()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Recipe ingredient not found " + command.getId())));
     }
 }
--- src/test/java/micha/udemy/recipeapp/service/IngredientServiceImplTest.java
+++ src/test/java/micha/udemy/recipeapp/service/IngredientServiceImplTest.java
@@ -1,11 +1,14 @@
 package micha.udemy.recipeapp.service;
 
 import micha.udemy.recipeapp.command.IngredientCommand;
+import micha.udemy.recipeapp.converter.IngredientCommandToIngredient;
 import micha.udemy.recipeapp.converter.IngredientToIngredientCommand;
+import micha.udemy.recipeapp.converter.UnitOfMeasureCommandToUnitOfMeasure;
 import micha.udemy.recipeapp.converter.UnitOfMeasureToUnitOfMeasureCommand;
 import micha.udemy.recipeapp.model.Ingredient;
 import micha.udemy.recipeapp.model.Recipe;
 import micha.udemy.recipeapp.repository.RecipeRepository;
+import micha.udemy.recipeapp.repository.UnitOfMeasureRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +20,7 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -27,15 +31,20 @@ public class IngredientServiceImplTest {
 
     @Mock
     RecipeRepository recipeRepository;
+    @Mock
+    UnitOfMeasureRepository unitOfMeasureRepository;
 
     IngredientToIngredientCommand ingredientToIngredientCommand;
+    IngredientCommandToIngredient ingredientCommandToIngredient;
 
     IngredientServiceImpl ingredientService;
 
     @BeforeEach
     public void setUp() {
         ingredientToIngredientCommand = new IngredientToIngredientCommand(new UnitOfMeasureToUnitOfMeasureCommand());
-        ingredientService = new IngredientServiceImpl(ingredientToIngredientCommand, recipeRepository);
+        ingredientCommandToIngredient = new IngredientCommandToIngredient(new UnitOfMeasureCommandToUnitOfMeasure());
+        ingredientService = new IngredientServiceImpl(ingredientToIngredientCommand, ingredientCommandToIngredient,
+            recipeRepository, unitOfMeasureRepository);
     }
 
     @Test
@@ -59,6 +68,28 @@ public class IngredientServiceImplTest {
         assertEquals(3L, ingredientCommand.getId());
         assertEquals(1L, ingredientCommand.getRecipeId());
         verify(recipeRepository, times(1)).findById(anyLong());
+    }
+
+    @Test
+    public void saveRecipeCommand() {
+        IngredientCommand command = new IngredientCommand();
+        command.setId(3L);
+        command.setRecipeId(2L);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(Optional.of(new Recipe()));
+
+        Recipe savedRecipe = new Recipe();
+        savedRecipe.addIngredient(new Ingredient());
+        savedRecipe.getIngredients().iterator().next().setId(3L);
+        when(recipeRepository.save(any())).thenReturn(savedRecipe);
+
+        //when
+        IngredientCommand savedCommand = ingredientService.saveIngredientCommand(command);
+
+        //then
+        assertEquals(Long.valueOf(3L), savedCommand.getId());
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, times(1)).save(any(Recipe.class));
     }
 
 }
